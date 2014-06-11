304af0451ed68e8d916968be077f9d0d9da628ec
Collin Van Dyck <collin@librato.com>
2014-06-11 09:13:24 -0700

    Added support for more builder properties

--- metrics-librato-dropwizard/src/main/java/io/dropwizard/metrics/LibratoReporterFactory.java
+++ metrics-librato-dropwizard/src/main/java/io/dropwizard/metrics/LibratoReporterFactory.java
@@ -4,43 +4,72 @@ import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.ScheduledReporter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.librato.metrics.HttpPoster;
 import com.librato.metrics.LibratoReporter;
+import com.librato.metrics.NingHttpPoster;
 
 import javax.validation.constraints.NotNull;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 @JsonTypeName("librato")
 public class LibratoReporterFactory extends BaseReporterFactory {
     @NotNull
     @JsonProperty
-    private String username = null;
+    private String username;
 
     @NotNull
     @JsonProperty
-    private String token = null;
+    private String token;
 
     @NotNull
     @JsonProperty
-    private String source = null;
+    private String source;
 
     @JsonProperty
-    private long timeout = 5;
+    private Long timeout;
 
     @JsonProperty
-    private String prefix = null;
+    private String prefix;
 
     @JsonProperty
-    private String name = null;
+    private String name;
+
+    @JsonProperty
+    private String libratoUrl;
+
+    @JsonProperty
+    private String sourceRegex;
+
+    @JsonProperty
+    private String prefixDelimiter;
 
     public ScheduledReporter build(MetricRegistry registry) {
-        return LibratoReporter.builder(registry, username, token, source)
-                .setTimeout(timeout, TimeUnit.SECONDS)
+        Pattern sourceRegexPattern = null;
+        if (sourceRegex != null) {
+            sourceRegexPattern = Pattern.compile(sourceRegex);
+        }
+        HttpPoster httpPoster = null;
+        if (libratoUrl != null) {
+            httpPoster = NingHttpPoster.newPoster(username, token, libratoUrl);
+        }
+        LibratoReporter.Builder builder = LibratoReporter.builder(registry, username, token, source)
                 .setPrefix(prefix)
                 .setRateUnit(getRateUnit())
                 .setDurationUnit(getDurationUnit())
                 .setName(name)
                 .setFilter(getFilter())
-                .build();
+                .setHttpPoster(httpPoster)
+                .setSourceRegex(sourceRegexPattern);
+
+        if (timeout != null) {
+            builder.setTimeout(timeout, TimeUnit.SECONDS);
+        }
+        if (prefixDelimiter != null) {
+            builder.setPrefix(prefixDelimiter);
+        }
+
+        return builder.build();
     }
 }
 
