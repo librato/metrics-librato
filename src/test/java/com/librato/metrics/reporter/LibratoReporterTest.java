5d57ae84d9308e24472263c1b9d9a16cc499bf43
Collin Van Dyck <collin@librato.com>
2016-11-15 17:33:24 -0500

    Meter test

--- src/test/java/com/librato/metrics/reporter/LibratoReporterTest.java
+++ src/test/java/com/librato/metrics/reporter/LibratoReporterTest.java
@@ -3,9 +3,11 @@ package com.librato.metrics.reporter;
 import com.codahale.metrics.*;
 import com.librato.metrics.client.*;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -22,19 +24,63 @@ public class LibratoReporterTest {
     SortedMap<String, Meter> meters = new TreeMap<String, Meter>();
     SortedMap<String, Timer> timers = new TreeMap<String, Timer>();
     LibratoClient client = mock(LibratoClient.class);
+    ReporterAttributes atts = new ReporterAttributes();
+    ArgumentCaptor<Measures> captor = ArgumentCaptor.forClass(Measures.class);
 
-    @Test
-    public void testUsesSourceRegexForGauges() throws Exception {
-        ReporterAttributes atts = new ReporterAttributes();
+    @Before
+    public void setUp() throws Exception {
         atts.sourceRegex = Pattern.compile("^(.*)--");
         atts.libratoClientFactory = new ILibratoClientFactory() {
             public LibratoClient build(ReporterAttributes atts) {
                 return client;
             }
         };
-        ArgumentCaptor<Measures> captor = ArgumentCaptor.forClass(Measures.class);
-        when(client.postMeasures(captor.capture())).thenReturn(new PostMeasuresResult());
+        when(client.postMeasures(captor.capture()))
+                .thenReturn(new PostMeasuresResult());
+    }
 
+    @Test
+    public void testMeter() throws Exception {
+        Meter meter = mock(Meter.class);
+        when(meter.getCount()).thenReturn(1L);
+        when(meter.getMeanRate()).thenReturn(2d);
+        when(meter.getOneMinuteRate()).thenReturn(3d);
+        when(meter.getFiveMinuteRate()).thenReturn(4d);
+        when(meter.getFifteenMinuteRate()).thenReturn(5d);
+        meters.put("foo", meter);
+        LibratoReporter reporter = new LibratoReporter(atts);
+        reporter.report(gauges, counters, histos, meters, timers);
+        HashSet<IMeasure> measures = new HashSet<IMeasure>(captor.getValue().getMeasures());
+        System.out.println("measures = " + measures);
+        assertThat(measures).containsOnly(
+                new GaugeMeasure("foo.count", 1),
+                new GaugeMeasure("foo.meanRate", meter.getMeanRate()),
+                new GaugeMeasure("foo.1MinuteRate", meter.getOneMinuteRate()),
+                new GaugeMeasure("foo.5MinuteRate", meter.getFiveMinuteRate()),
+                new GaugeMeasure("foo.15MinuteRate", meter.getFifteenMinuteRate()));
+    }
+
+    @Test
+    public void testHisto() throws Exception {
+        Histogram histo = new Histogram(new UniformReservoir());
+        histo.update(42);
+        histos.put("foo", histo);
+        LibratoReporter reporter = new LibratoReporter(atts);
+        reporter.report(gauges, counters, histos, meters, timers);
+        HashSet<IMeasure> measures = new HashSet<IMeasure>(captor.getValue().getMeasures());
+        assertThat(measures).containsOnly(
+                new GaugeMeasure("foo", 42, 1, 42, 42),
+                new GaugeMeasure("foo.count", 1),
+                new GaugeMeasure("foo.median", 42),
+                new GaugeMeasure("foo.75th", 42),
+                new GaugeMeasure("foo.95th", 42),
+                new GaugeMeasure("foo.98th", 42),
+                new GaugeMeasure("foo.99th", 42),
+                new GaugeMeasure("foo.999th", 42));
+    }
+
+    @Test
+    public void testUsesSourceRegexForGauges() throws Exception {
         gauges.put("ec2--foo", new Gauge() {
             public Object getValue() {
                 return 42;
@@ -46,21 +92,11 @@ public class LibratoReporterTest {
         Measures measures = captor.getValue();
         assertThat(measures.getMeasures()).hasSize(1);
         assertThat(measures.getMeasures().get(0)).isEqualTo(
-                new GaugeMeasure("name", 42).setSource("ec2"));
+                new GaugeMeasure("foo", 42).setSource("ec2"));
     }
 
     @Test
     public void testUsesSourceRegexForMeters() throws Exception {
-        ReporterAttributes atts = new ReporterAttributes();
-        atts.sourceRegex = Pattern.compile("^(.*)--");
-        atts.libratoClientFactory = new ILibratoClientFactory() {
-            public LibratoClient build(ReporterAttributes atts) {
-                return client;
-            }
-        };
-        ArgumentCaptor<Measures> captor = ArgumentCaptor.forClass(Measures.class);
-        when(client.postMeasures(captor.capture())).thenReturn(new PostMeasuresResult());
-
         Meter meter = new Meter();
         meters.put("ec2--foo", meter);
         meter.mark();
