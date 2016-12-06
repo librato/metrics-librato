f35dd7f75c7a88de9da5dd576b05f79a2846af52
Collin Van Dyck <collin@librato.com>
2016-12-06 14:12:07 -0500

    Make 1.6 compliant

--- src/main/java/com/librato/metrics/reporter/LibratoReporter.java
+++ src/main/java/com/librato/metrics/reporter/LibratoReporter.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import java.net.SocketTimeoutException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 import java.util.SortedMap;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
